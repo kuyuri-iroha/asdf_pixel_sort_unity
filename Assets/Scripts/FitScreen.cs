960fe3addb18eec2b63efc807120cd8ac7674f1c
Kuyuri-Iroha <kuyuri.iroha@gmail.com>
2019-12-23 00:40:26 +0900

    2048*2048以下制限のピクセルソートの追加
    サイズが2048*2048以下であることを条件に、自由な画像サイズで実行可能なASDF Pixel Sortを実装して追加した。

---
+++ Assets/Scripts/FitScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitScreen : MonoBehaviour
+{
+    [SerializeField]
+    new Camera camera = null;
+
+    [SerializeField]
+    float distance = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(camera == null)
+        {
+            camera = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = camera.transform.forward.normalized * distance + camera.transform.position;
+
+        transform.LookAt(camera.transform.position);
+
+        const float PLANE_SCALE = 0.1f;
+        var frustumHeight = 2.0f * distance * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad) * PLANE_SCALE;
+        var frustumWidth = frustumHeight * camera.aspect;
+
+        var scale = new Vector3( frustumWidth, frustumHeight, 1.0f );
+        transform.localScale = scale;
+    }
+}
---
+++ Assets/Scripts/PixelSort.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelSort : MonoBehaviour
+{
+    [SerializeField]
+    ComputeShader sorterKernel = null;
+
+    [SerializeField]
+    Texture2D src = null;
+
+    [SerializeField, Range(0.0f, 1.0f)]
+    float brightnessThreshold = 0.0f;
+
+    int kernelIndexHorizontal;
+    int kernelIndexVertical;
+
+    RenderTexture[] buffers;
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kernelIndexHorizontal = sorterKernel.FindKernel("PixelSortKernelHorizontal");
+        kernelIndexVertical = sorterKernel.FindKernel("PixelSortKernelVertical");
+
+        buffers = new RenderTexture[2];
+        for(var i = 0; i < buffers.Length; i++)
+        {
+            buffers[i] = new RenderTexture(src.width, src.height, 0, RenderTextureFormat.ARGB32);
+            buffers[i].enableRandomWrite = true;
+            buffers[i].Create();
+        }
+
+        mat = GetComponent<Renderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Graphics.CopyTexture(src, buffers[0]);
+
+        sorterKernel.SetFloat("brightnessThreshold", brightnessThreshold);
+
+        int count = 0;
+        for(int i = 0; i < src.width; i++)
+        {
+            sorterKernel.SetFloat("iteration", i);
+            sorterKernel.SetTexture(kernelIndexHorizontal, "src", buffers[count % 2]);
+            sorterKernel.SetTexture(kernelIndexHorizontal, "dest", buffers[(count + 1) % 2]);
+            sorterKernel.Dispatch(kernelIndexHorizontal, 1, src.height, 1);
+
+            count++;
+        }
+
+        Graphics.CopyTexture(src, buffers[count % 2]);
+        count = 0;
+        for(int i = 0; i < src.height; i++)
+        {
+            sorterKernel.SetFloat("iteration", i);
+            sorterKernel.SetTexture(kernelIndexVertical, "src", buffers[count % 2]);
+            sorterKernel.SetTexture(kernelIndexVertical, "dest", buffers[(count + 1) % 2]);
+            sorterKernel.Dispatch(kernelIndexVertical, src.width, 1, 1);
+
+            count++;
+        }
+
+        mat.SetTexture("_UnlitColorMap", buffers[count % 2]);
+    }
+}
